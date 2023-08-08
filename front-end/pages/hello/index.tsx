f1d535af52af08652cae90941eb09ed1dbd7619c
Joshua Salcedo <129421068+swaswa14@users.noreply.github.com>
2023-08-09 03:01:33 +0800

    Added local dev.

--- front-end/pages/hello/index.tsx
+++ front-end/pages/hello/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import {useEffect, useState} from "react";
+
+export default function HelloPage() {
+    const [helloResponse, setHelloResponse] = useState<string|null>(null)
+
+    useEffect(() => {
+        fetch('/api/hello-world')
+            .then((res) => res.text())
+            .then((data) => {
+                setHelloResponse(data)
+            })
+    }, [])
+
     return (
         <>
             Hello world!
+            Response from server call to /api/hello-world: {helloResponse}
         </>
     )
 }
