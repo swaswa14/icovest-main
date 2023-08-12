10fa0424e26bd260404a54fc3a687254b9c9df38
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-08-12 09:29:14 +0800

    added dto

--- back-end/user-feature/src/main/java/com/icovest/backend/userfeature/entity/UserDto.java
+++ back-end/user-feature/src/main/java/com/icovest/backend/userfeature/entity/UserDto.java
@@ -1,4 +1,13 @@
 package com.icovest.backend.userfeature.entity;
 
-public record UserDto() {
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+public record UserDto(
+        Long id,
+        String username,
+        String email,
+        List<GrantedAuthority> authorities
+) {
 }
